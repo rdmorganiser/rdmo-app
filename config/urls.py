2763239fe8e13a252f3279bd8476754e9bc177f6
Jochen Klar <mail@jochenklar.de>
2021-01-24 14:49:54 +0100

    Add settings.DEBUG_TOOLBAR

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-import debug_toolbar
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -12,9 +12,12 @@ urlpatterns = [
     path('api/v1/', include('rdmo.core.urls.v1')),
     path('api/v1/', include('rdmo.core.urls.swagger')),
 
-    path('admin/', admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls)),
+    path('admin/', admin.site.urls)
 ]
+
+if settings.DEBUG_TOOLBAR:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
 
 handler400 = 'rdmo.core.views.bad_request'
 handler403 = 'rdmo.core.views.forbidden'
